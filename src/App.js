7833669fbb2618bbe83611519b76c9e096e56b19
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-09-04 18:14:18 +0530

    many props use

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import HomePage from './pages/HomePage';
 function App() {
   return (
     <div className="App">
-      <HomePage name="piyumal"/>
-      <HomePage name="nipuna"/>
-      <HomePage name="kamal"/>
+      <HomePage name="piyumal" age="10"/>
+      <HomePage name="nipuna" age="20"/>
+      <HomePage name="kamal" age="30"/>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomePage(props) {
   return (
     <div>
-      <h1>his name is {props.name}</h1>
+      <h1>his name is {props.name} age is {props.age}</h1>
     </div>
   )
 }
