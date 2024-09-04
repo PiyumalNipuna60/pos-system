5457506363e5bff1f4dce3b24fadac9bbff7b681
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-09-04 22:35:16 +0530

    useState use project

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incriment=()=> {
+    setCount(count + 1);
+  }
+
+  const decriment=()=> {
+    setCount(count - 1);
+  }
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <h1 className="header">My Count</h1>
+      <h2 className="count">count is {count}</h2>
+      <button onClick={decriment} className="button">
+        -
+      </button>
+      <button onClick={incriment} className="button">
+        +
+      </button>
+    </>
   );
 }
 
